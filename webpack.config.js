90255d337cceea74eb375c7ae1da157265ce42ac
HeadInoueTakuma <inoue.takuma@pro.headwaters.co.jp>
2023-03-09 20:19:15 +0900

    1-4が終了

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,18 @@ const path = require("path");
 
 module.exports = {
   // 最初に読み込むファイルを指定
-  entry: "./src/index.ts",
+  entry: "./src/index.tsx",
   module: {
     rules: [
       {
-        test: /\.ts$/,
+        test: /\.tsx?$/,
         use: "ts-loader",
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: [".ts"],
+    extensions: [".js", ".ts", ".tsx"],
   },
   output: {
     path: path.resolve(__dirname, "dist"),
